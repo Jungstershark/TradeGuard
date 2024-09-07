8d90fedd525616992390f7f2230c32eeba4ee32f
Asher <110836734+influasher@users.noreply.github.com>
2024-09-07 19:20:21 +0800

    initial commit for records instance

---
+++ backend/records/src/main/java/com/code9impact/records/domain/Record.java
@@ -0,0 +1,23 @@
+package com.code9impact.records.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "records")
+public class Record {
+    @Id
+    @GeneratedValue
+    private long id;
+    private String title;
+}
