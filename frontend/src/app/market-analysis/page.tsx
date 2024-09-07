7e390b7f4303ee27a24d9bd76929a20142112931
Cohii <hengjunxiangg@gmail.com>
2024-09-07 16:51:30 +0800

    Add InstrumentTable component

--- frontend/src/app/market-analysis/page.tsx
+++ frontend/src/app/market-analysis/page.tsx
@@ -2,6 +2,7 @@
 
 import { useState } from 'react'
 import TextSearch from "@/app/components/TextSearch";
+import InstrumentTable, { testData } from "@/app/market-analysis/components/InstrumentTable";
 
 const initialFilters = {
     "Instrument Group" : "",
@@ -16,6 +17,8 @@ const initialFilters = {
 export default function Page() {
     const [filters, setFilters] = useState(initialFilters)
 
+    const [data, setData] = useState(testData)
+
     const handleChange = (e) => {
         const newFilters = {
             ...filters,
@@ -25,20 +28,40 @@ export default function Page() {
         console.log(newFilters)
     }
 
-    return (
-        <div className="p-10 w-full min-h-full">
-            <ul className="grid grid-cols-2 w-full h-1">
-                {Object.entries(filters).map((filter, idx) => {
-                    const label: string = filter[0]
-                    const input: string = filter[1]
+    const handleCheckboxChange = (id) => {
+        const newData = [...data].map(instrumentData => {
+            if (instrumentData.Id === id) {
+                return {
+                    ...instrumentData,
+                    IsSelected: !instrumentData.IsSelected
+                }
+            } else {
+                return instrumentData
+            }
+        })
+        console.log(newData)
+        setData(newData);
+    };
 
-                    return (
-                        <li key={idx}>
-                            <TextSearch label={label} text={input} handleChange={handleChange}/>
-                        </li>
-                    )
-                })}
-            </ul>
+    return (
+        <div className="flex flex-col p-10 w-full min-h-full">
+            <div>
+                <ul className="grid grid-cols-2 w-full h-min">
+                    {Object.entries(filters).map((filter, idx) => {
+                        const label: string = filter[0]
+                        const input: string = filter[1]
+
+                        return (
+                            <li key={idx}>
+                                <TextSearch label={label} text={input} handleChange={handleChange}/>
+                            </li>
+                        )
+                    })}
+                </ul>
+            </div>
+            <div className="mt-10">
+                <InstrumentTable rows={data} handleCheckboxChange={handleCheckboxChange} />
+            </div>
         </div>
     )
 }
