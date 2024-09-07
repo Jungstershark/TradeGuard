7830c1de03e1047b9322b72f767ffb3525190e1e
Asher <110836734+influasher@users.noreply.github.com>
2024-09-08 02:06:53 +0800

    first commit auth service

---
+++ backend/auth-service/src/main/java/com/code9impact/auth_service/auth/AuthenticationResponse.java
@@ -0,0 +1,16 @@
+package com.code9impact.auth_service.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthenticationResponse {
+
+    private String token;
+
+}
