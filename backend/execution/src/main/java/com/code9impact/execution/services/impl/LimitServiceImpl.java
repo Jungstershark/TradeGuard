05b8fc2d594c09b5882c11cb270ae6725116d3ff
Jungstershark <ongjy1503@gmail.com>
2024-09-07 17:14:10 +0800

    Implemented controller defining API endpoints /limits and /limits/{id}, implemented service, service interface and edited application properties to define unique port for execution microservice

--- backend/execution/src/main/java/com/code9impact/execution/services/impl/LimitServiceImpl.java
+++ backend/execution/src/main/java/com/code9impact/execution/services/impl/LimitServiceImpl.java
@@ -1,18 +1,33 @@
-package com.code9impact.execution.services;
+package com.code9impact.execution.services.impl;
 
 import com.code9impact.execution.domains.LimitObject;
 import com.code9impact.execution.repositories.LimitRepository;
+import com.code9impact.execution.services.LimitService;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
-@Service
-public class LimitService {
+@Component
+public class LimitServiceImpl implements LimitService {
 
-    private LimitRepository limitRepository;
+    private final LimitRepository limitRepository;
 
-    public List<LimitObject> getAllLimits() {
+    public LimitServiceImpl(LimitRepository limitRepository) {
+        this.limitRepository = limitRepository;
+    }
+
+    @Override
+    public Iterable<LimitObject> getAllLimits() {
         return limitRepository.findAll();
     }
+
+    @Override
+    public Optional<LimitObject> getLimitBy(String id) {
+        return limitRepository.findById(id);
+    }
+
+
 }
-}
+
