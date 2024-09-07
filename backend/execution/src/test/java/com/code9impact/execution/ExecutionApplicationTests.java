a14f699a7a793cb3bceaaa3e4dcba692f123f376
Asher <110836734+influasher@users.noreply.github.com>
2024-09-07 12:29:08 +0800

    added database to properties

---
+++ backend/execution/src/test/java/com/code9impact/execution/ExecutionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.code9impact.execution;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExecutionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
